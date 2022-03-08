b2c0523d780b9bf95a737bec2e8c1967a1027b5c
Afonso Baptista <afonso.7.baptista@gmail.com>
2022-03-08 11:00:15 +0000

    Add GET request for movies JSON data

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 const express = require('express');
 const app = express();
 
+let topTenMovies = [
+    {
+        title: 'The Shawshank Redemption',
+        director: 'Frank Darabont'
+    },
+    {
+        title: 'The Godfather',
+        director: 'Francis Ford Coppola'
+    },
+    {
+        title: 'The Godfather: Part II',
+        director: 'Francis Ford Coppola'
+    },
+    {
+        title: 'The Dark Knight',
+        director: 'Christopher Nolan'
+    },
+    {
+        title: '12 Angry Men',
+        director: 'Sidney Lumet'
+    },
+    {
+        title: 'Schindler\'s List',
+        director: 'Steven Spielberg'
+    },
+    {
+        title: 'The Lord of the Rings: The Return of the King',
+        director: 'Peter Jackson'
+    },
+    {
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino'
+    },
+    {
+        title: 'The Good, the Bad and the Ugly',
+        director: 'Sergio Leone'
+    },
+    {
+        title: 'The Lord Of The Rings: The Fellowship Of The Rings',
+        director: 'Peter Jackson'
+    }
+];
+
+// GET requests
+app.get('/movies', (req, res) => {
+    res.json(topTenMovies);
+});
